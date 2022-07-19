561246e832a55d2905ca6ccadfbb3e3eeb7f4210
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-07-19 14:56:50 -0300

    Ajuste final da lógica de importação dos dados

--- fbd_scripts/prc-etl-despesa.sql
+++ fbd_scripts/prc-etl-despesa.sql
@@ -23,70 +23,69 @@ BEGIN
     	FROM CARGA_DESPESA 
     	WHERE COD_DOCUMENTO NOT IN (
     		SELECT COD_DOCUMENTO COLLATE utf8mb4_0900_as_ci AS COD FROM DESPESA
-    	) ;
+    	) AND SENADOR IN (
+    		SELECT DISTINCT s.NOME COLLATE utf8mb4_0900_as_ci AS NOME_SENADOR FROM SENADOR s);
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    SELECT ANO, MES, SENADOR, TIPO_DESPESA, CNPJ_CPF, FORNECEDOR, DOCUMENTO, DATA_REEMBOLSO, DETALHAMENTO, VALOR_REEMBOLSADO, COD_DOCUMENTO 
-    	FROM CARGA_DESPESA 
-    	WHERE COD_DOCUMENTO NOT IN (
-    		SELECT COD_DOCUMENTO COLLATE utf8mb4_0900_as_ci AS COD FROM DESPESA
-    	) ;
-
-   	SELECT 'Iniciando pesquisa';
+   	
+    SELECT CONCAT('TOTAL DE REGISTROS: ',FOUND_ROWS()); 
+	SELECT 'Iniciando cadastro...';
     OPEN curs;
    
 	read_loop: LOOP
     -- WHILE (done != true)DO
-    	SELECT CONCAT('TOTAL DE REGISTROS: ',FOUND_ROWS()); 
-    	SELECT 'Iniciando loop';
-    	-- SET done = true;
+    	SET done = false;
         FETCH curs INTO v_ano, v_mes, v_senador, v_tipoDespesa, v_cnpjCpf, v_fornecedor, v_documento, v_dataReembolso, v_detalhamento, v_valorReembolsado, v_codDocumento;
         IF done THEN
         	LEAVE read_loop;
         END IF;
        -- Verifica se o Senador já está na base, para recuperar o ID dele
-       	SELECT CONCAT('Pesquisando o Senador ', v_senador); 
+       	-- SELECT CONCAT('Pesquisando o Senador ', v_senador); 
 		SELECT ID_SENADOR INTO idSenador FROM SENADOR WHERE UPPER(NOME) = UPPER(v_senador) COLLATE utf8mb4_0900_as_ci;
-		SELECT concat('Recuperou o senador com id ', idSenador);
+		-- SELECT concat('Recuperou o senador com id ', idSenador);
 		IF (idSenador IS NOT NULL AND idSenador > 0) THEN
 			START TRANSACTION;
 		
 			-- Verifica se foi informado um fornecedor
 			IF (v_cnpjCpf IS NOT NULL AND TRIM(v_cnpjCpf) <> '') THEN
-				BEGIN 
-				-- Verifica se o fornecedor já está na base. Se não estiver, insere
-					SET cpfCnpjLimpo = REPLACE(REPLACE(REPLACE(v_cnpjCPf, '.', ''),'-',''),'/','');
-					SELECT CONCAT('Verificando fornecedor com cpfCnpj ', cpfCnpjLimpo);
-					SELECT ID_FORNECEDOR INTO idFornecedor FROM FORNECEDOR WHERE CPF_CNPJ = cpfCnpjLimpo COLLATE utf8mb4_0900_as_ci;
-					SELECT CONCAT('Fornecedor: ', idFornecedor);
-					IF (idFornecedor IS NULL) THEN
-						INSERT INTO FORNECEDOR (NOME, CPF_CNPJ) VALUES (v_fornecedor, cpfCnpjLimpo);
-						SET idFornecedor = LAST_INSERT_ID();
-						SELECT CONCAT('Fornecedor ', v_fornecedor, ' inserido com id ', idFornecedor);
-					END IF;
-				END;
+			-- Verifica se o fornecedor já está na base. Se não estiver, insere
+				SET cpfCnpjLimpo = REPLACE(REPLACE(REPLACE(v_cnpjCPf, '.', ''),'-',''),'/','');
+				-- SELECT CONCAT('Verificando fornecedor com cpfCnpj ', cpfCnpjLimpo);
+				SELECT ID_FORNECEDOR INTO idFornecedor FROM FORNECEDOR WHERE CPF_CNPJ = cpfCnpjLimpo COLLATE utf8mb4_0900_as_ci;
+				-- SELECT CONCAT('Fornecedor: ', idFornecedor);
+				IF (idFornecedor IS NULL) THEN
+					INSERT INTO FORNECEDOR (NOME, CPF_CNPJ) VALUES (v_fornecedor, cpfCnpjLimpo);
+					SET idFornecedor = LAST_INSERT_ID();
+					-- SELECT CONCAT('Fornecedor ', v_fornecedor, ' inserido com id ', idFornecedor);
+				END IF;
 			END IF;
 		
 			-- Verifica se o tipo de despesa já está na base
-			SELECT CONCAT('Verificando tipo de despesa', v_tipoDespesa);
+			-- SELECT CONCAT('Verificando tipo de despesa', v_tipoDespesa);
 			SELECT ID_TIPO_DESPESA INTO idTipoDespesa FROM TIPO_DESPESA WHERE UPPER(DESCRICAO) = TRIM(UPPER(v_tipoDespesa)) COLLATE utf8mb4_0900_as_ci;
-			SELECT CONCAT('Tipo de despesa: ', idFornecedor);
+			-- SELECT CONCAT('Tipo de despesa: ', idFornecedor);
 			IF (idTipoDespesa IS NULL) THEN
-				BEGIN
-					INSERT INTO TIPO_DESPESA (DESCRICAO) VALUES (v_tipoDespesa);
-					SET idTipoDespesa = LAST_INSERT_ID(); 
-					SELECT CONCAT('Tipo de despesa ', v_tipoDespesa, ' inserido com id ', idTipoDespesa);
-				END;
+				INSERT INTO TIPO_DESPESA (DESCRICAO) VALUES (v_tipoDespesa);
+				SET idTipoDespesa = LAST_INSERT_ID(); 
+				-- SELECT CONCAT('Tipo de despesa ', v_tipoDespesa, ' inserido com id ', idTipoDespesa);
 			END IF;
 			
 			-- SELECT CONCAT ('Ano: ', v_ano);
 			-- Insere os dados na tabela DESPESA
 			INSERT INTO DESPESA (ANO, MES, ID_SENADOR, ID_FORNECEDOR, ID_TIPO_DESPESA, DATA_REEMBOLSO, DETALHAMENTO , DOCUMENTO, COD_DOCUMENTO)
 			VALUES (v_ano, v_mes, idSenador, idFornecedor, idTipoDespesa, v_dataReembolso, v_detalhamento ,v_documento, v_codDocumento);
+			-- Limpa as variáveis para garantir que a verificação será feita corretamente
+			SET idFornecedor = NULL;
+			SET idTipoDespesa = NULL;
+			
 			
 			COMMIT;
         END IF;
+       	-- Limpa o senador para garantir outro loop
+       	SET idSenador = NULL;
 
 	END LOOP;
     CLOSE curs;
+    SELECT 'Importação terminada!'; 
+   
 
 END
