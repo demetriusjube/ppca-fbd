ee1fc5520862cf9030dc8eb3ba5ebf8f5b1769ce
vanerven <109423338+vanerven@users.noreply.github.com>
2022-07-27 11:25:25 -0300

    Update trg-carga-senador.sql

--- fbd_scripts/trg-carga-senador.sql
+++ fbd_scripts/trg-carga-senador.sql
@@ -1,10 +1,9 @@
-delimiter //
-
+delimiter $$
 CREATE DEFINER=`root`@`localhost` TRIGGER TRG_CARGA_SENADOR
 AFTER INSERT
 ON carga_senador FOR EACH row
-BEGIN
-	DECLARE v_nome_senador varchar(255) default null;
+begin
+	DECLARE v_nome_senador varchar(255) default '';
     DECLARE v_id_senador int default 0;
     DECLARE v_id_mandato int default 0;
    
@@ -21,7 +20,9 @@ BEGIN
     	INSERT INTO MANDATO (ID_SENADOR, ESTADO, PERIODO, LEGISLATURA, PARTIDO) VALUES (V_ID_SENADOR, new.uf, new.PERIODO, new.LEGISLATURA, new.PARTIDO);
     	SELECT m.id_mandato INTO v_id_mandato FROM fbd.mandato m WHERE m.ID_SENADOR = v_id_senador AND m.LEGISLATURA = new.legislatura;
         INSERT INTO MANDATO_LEGISLATURA (ID_MANDATO, NR_LEGISLATURA) VALUES (v_id_mandato, new.LEGISLATURA);
-    end if;TRG_CARGA_SENADOR
- END //
-
-delimiter ;
+    end if;
+    
+ END 
+ $$
+ 
+ delimiter ;
