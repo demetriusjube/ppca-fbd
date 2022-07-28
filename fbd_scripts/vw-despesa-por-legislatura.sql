03e4422bf93ee3d0dfa5de3286d89f984b23f152
vanerven <109423338+vanerven@users.noreply.github.com>
2022-07-28 20:29:34 -0300

    Update vw-despesa-por-legislatura.sql

--- fbd_scripts/vw-despesa-por-legislatura.sql
+++ fbd_scripts/vw-despesa-por-legislatura.sql
@@ -1,20 +1,34 @@
 DELIMITER $$
 
-create view FBD.VW_DESPESA_POR_LEGISLATURA AS
-select tp.DESCRICAO as DESCRICAO_DESPESA, sum(d.VALOR_REEMBOLSADO) as VALOR_TOTAL, concat(l.ANO_INICIO, " - ", l.ANO_FIM) as LEGISLATURA  
-from fbd.tipo_despesa tp, fbd.despesa d, fbd.senador s, 
-     fbd.fornecedor f, fbd.mandato m, fbd.legislatura l, 
-     fbd.mandato_legislatura ml  
-where tp.ID_TIPO_DESPESA = d.ID_TIPO_DESPESA and 
-      d.ID_SENADOR = s.ID_SENADOR and 
-      d.ID_FORNECEDOR = f.ID_FORNECEDOR and 
-      s.ID_SENADOR = m.ID_SENADOR and 
-      m.ID_MANDATO = ml.ID_MANDATO and 
-      m.LEGISLATURA = ml.NR_LEGISLATURA and 
-      ml.NR_LEGISLATURA = l.NR_LEGISLATURA and 
-      (d.ANO between l.ANO_INICIO and l.ANO_FIM)
-group by tp.ID_TIPO_DESPESA, l.NR_LEGISLATURA 
-order by periodo desc, valor_total desc;
+create or replace
+algorithm = UNDEFINED view `fbd`.`vw_despesa_por_legislatura` as
+select
+    `tp`.`DESCRICAO` as `DESCRICAO_DESPESA`,
+    sum(`d`.`VALOR_REEMBOLSADO`) as `VALOR_TOTAL`,
+    concat(`l`.`ANO_INICIO`, ' - ', `l`.`ANO_FIM`) as `LEGISLATURA`
+from
+    ((((((`fbd`.`tipo_despesa` `tp`
+join `fbd`.`despesa` `d`)
+join `fbd`.`senador` `s`)
+join `fbd`.`fornecedor` `f`)
+join `fbd`.`mandato` `m`)
+join `fbd`.`legislatura` `l`)
+join `fbd`.`mandato_legislatura` `ml`)
+where
+    ((`tp`.`ID_TIPO_DESPESA` = `d`.`ID_TIPO_DESPESA`)
+        and (`d`.`ID_SENADOR` = `s`.`ID_SENADOR`)
+            and (`d`.`ID_FORNECEDOR` = `f`.`ID_FORNECEDOR`)
+                and (`s`.`ID_SENADOR` = `m`.`ID_SENADOR`)
+                    and (`m`.`ID_MANDATO` = `ml`.`ID_MANDATO`)
+                        and (`m`.`LEGISLATURA` = `ml`.`NR_LEGISLATURA`)
+                            and (`ml`.`NR_LEGISLATURA` = `l`.`NR_LEGISLATURA`)
+                                and (`d`.`ANO` between `l`.`ANO_INICIO` and `l`.`ANO_FIM`))
+group by
+    `tp`.`ID_TIPO_DESPESA`,
+    `l`.`NR_LEGISLATURA`
+order by
+    `m`.`PERIODO` desc,
+    `VALOR_TOTAL` desc;
 
 $$
 
