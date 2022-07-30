8c63c3b92de150f1cf522720afc8b03e27d9f7ea
vanerven <109423338+vanerven@users.noreply.github.com>
2022-07-29 21:09:15 -0300

    Update consuta3-fornecedor-tipo-despesa-fixos-custos-senadores-variaveis.sql

--- fbd_scripts/consuta3-fornecedor-tipo-despesa-fixos-custos-senadores-variaveis.sql
+++ fbd_scripts/consuta3-fornecedor-tipo-despesa-fixos-custos-senadores-variaveis.sql
@@ -11,14 +11,12 @@ from (
 	from fbd.tipo_despesa td, fbd.despesa d, fbd.fornecedor f, fbd.senador s 
 	where td.ID_TIPO_DESPESA = d.ID_TIPO_DESPESA and d.ID_FORNECEDOR = f.ID_FORNECEDOR and d.ID_SENADOR = s.ID_SENADOR 
 	group by td.ID_TIPO_DESPESA, f.ID_FORNECEDOR, s.ID_SENADOR
-#	having valor_medio > 100 #and count(mes) = 1
 	) u1, (
 	select f.id_fornecedor, f.NOME as fornecedor, td.ID_TIPO_DESPESA, td.DESCRICAO as despesa, s.ID_SENADOR , s.NOME as senador, 
 	round((sum(d.VALOR_REEMBOLSADO) / count(d.MES)),2) as valor_medio, sum(d.VALOR_REEMBOLSADO) as valor_total, count(d.mes) as nr_parcelas
 	from fbd.tipo_despesa td, fbd.despesa d, fbd.fornecedor f, fbd.senador s
 	where td.ID_TIPO_DESPESA = d.ID_TIPO_DESPESA and d.ID_FORNECEDOR = f.ID_FORNECEDOR and d.ID_SENADOR = s.ID_SENADOR
 	group by td.ID_TIPO_DESPESA, f.ID_FORNECEDOR, s.ID_SENADOR
-#	having valor_medio < 100 #and count(mes) = 1
 	) u2, fbd.senador s1, fbd.senador s2
 where u1.id_fornecedor = u2.id_fornecedor and u1.id_tipo_despesa = u2.id_tipo_despesa and u1.id_senador != u2.id_senador and
       u1.id_senador = s1.ID_SENADOR and u2.id_senador = s2.id_senador and 
